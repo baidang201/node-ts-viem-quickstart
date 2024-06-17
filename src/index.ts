ce8608c5219fd5600474c6a24f8985747a65d9b0
Li Smith <jinrishouji@sina.com>
2024-06-17 20:51:44 +0800

    update database logic

--- src/index.ts
+++ src/index.ts
@@ -1,85 +1,92 @@
-import {
-  createPublicClient,
-  createWalletClient,
-  formatEther,
-  formatUnits,
-  getContract,
-  http,
-  parseAbi,
-  parseEther,
-  parseUnits,
-} from 'viem';
-import { mainnet } from 'viem/chains';
-import { privateKeyToAccount } from 'viem/accounts';
+import mempoolJS from '@mempool/mempool.js';
+import { Tx } from '@mempool/mempool.js/lib/interfaces/bitcoin/transactions';
 
-async function main() {
-  // 1. create public (read) and wallet (write) client with default rpc
-  const read = createPublicClient({ chain: mainnet, transport: http(), batch: { multicall: true } });
-  const write = createWalletClient({ chain: mainnet, transport: http() });
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
-  // or with custom rpc
-  // const rpc = 'https://rpc.ankr.com/eth';
-  // const read = createPublicClient({ chain: mainnet, transport: http(rpc), batch: { multicall: true } });
-  // const write = createWalletClient({ chain: mainnet, transport: http(rpc) });
+const {
+  bitcoin: { blocks, websocket },
+} = mempoolJS({
+  hostname: 'mempool.space',
+  network: 'testnet',
+});
 
-  // 2.1 basic: read block, check rpc out of sync
-  const block = await read.getBlock();
-  const now = BigInt(Math.floor(Date.now() / 1000));
-  if (now - block.timestamp > 30) throw Error('rpc out of sync');
+const ws = websocket.initServer({
+  options: ['blocks'],
+});
 
-  // 2.2 basic: read ether balance
-  const address = '0x388C818CA8B9251b393131C08a736A67ccB19297';
-  const balance = await read.getBalance({ address });
-  console.log(`balance: ${formatEther(balance)}`);
+ws.on('message', async function incoming(data) {
+  const res = JSON.parse(data.toString());
+  if (res.block) {
+    console.log('@@@ res.block \n');
+    console.log(res.block);
 
-  // 2.3 basic: send ether
-  const account = privateKeyToAccount('0x...');
-  const hash = await write.sendTransaction({ account, to: address, value: parseEther('1.1') });
-  console.log(`send ether tx: ${hash}`);
+    const blockStatus = await blocks.getBlockStatus({ hash: res.block.id });
+    console.log('@@@ status', blockStatus);
 
-  // 3.1 contract: abi
-  const abi = parseAbi([
-    'function name() public view returns (string memory)',
-    'function symbol() view returns (string memory)',
-    'function decimals() view returns (uint8)',
-    'function balanceOf(address account) public view returns (uint256)',
-    'function transfer(address to, uint256 value) public returns (bool)',
-    'event Transfer(address indexed from, address indexed to, uint256 value)',
-  ]);
+    const blockTxs = await blocks.getBlockTxs({ hash: res.block.id });
+    console.log('@@@ blockTxs0', blockTxs);
 
-  // 3.1 contract: get instance
-  const contract = getContract({
-    address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
-    abi,
-    client: { public: read, wallet: write },
-  });
+    // todo get mapping(uint256 => Escrow) escrows; from eth
 
-  // 3.2 contract: single query
-  const name = await contract.read.name();
-  console.log(`token name: ${name}`);
+    for (var tx in blockTxs) {
+      if (1 === tx.vin.length) {
+        let vin = tx.vin[0];
 
-  // 3.2 contract: batch queries
-  const [symbol, decimals, tokenBalance] = await Promise.all([
-    contract.read.symbol(),
-    contract.read.decimals(),
-    contract.read.balanceOf(['0xcEe284F754E854890e311e3280b767F80797180d']),
-  ]);
-  console.log(`${symbol} balance of ${address}: ${formatUnits(tokenBalance, decimals)}`);
+        for (var item in tx.vout) {
+          let origin = vin.prevout.scriptpubkey;
+          let destination = item.scriptpubkey;
+          let amount = item.value;
 
-  // 3.3 contract: write
-  const to = '0x4838B106FCe9647Bdf1E7877BF73cE8B0BAD5f97';
-  const tx = await contract.write.transfer([to, parseUnits('1', decimals)], { account });
-  console.log(`transfer erc20 tx: ${tx}`);
+          // todo check "origin to destination with the specified amount" from eth data
+          await prisma.transactions.create({
+            data: {
+              origin: origin,
+              request_number: 1,
+              quote_number: 2,
+              amount: amount,
+              destination: destination,
+              expiry: 100,
+              fulfilled: false,
+            },
+          });
 
-  // 3.4 contract: read event
-  contract.watchEvent.Transfer({}, { onLogs: (logs) => console.log(logs) });
+          // const allUsers = await prisma.transactions.findMany({
+          //   select: {
+          //     origin: true,
+          //     destination: true,
+          //   },
+          // });
 
-  await new Promise(function () {});
-}
+          //console.dir(allUsers, { depth: null });
+
+          // todo  update contract data
+        }
+      }
+    }
+  }
+  // if (res.mempoolInfo) {
+  //   console.log('@@@ res.mempoolInfo \n');
+  //   console.log(res.mempoolInfo);
+  // }
+  // if (res.transactions) {
+  //   console.log('@@@ res.transactions \n');
+  //   console.log(res.transactions);
+  // }
+  // if (res['mempool-blocks']) {
+  //   console.log('@@@ res.mempool-blocks \n');
+  //   console.log(res['mempool-blocks']);
+  // }
+});
+
+async function main() {}
 
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
     process.exit(1);
   });
