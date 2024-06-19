e7bbc7c711578ffda6dbb7a3e78ef490a0dd2b9a
Li Smith <jinrishouji@sina.com>
2024-06-19 20:12:21 +0800

    1 add blocktimestamp check

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import mempoolJS from '@mempool/mempool.js';
 import { PrismaClient } from '@prisma/client';
 import { getEscrowCount, getEscrow, fulfillRequest, getRequest } from './eth';
+import * as base58 from 'bs58';
+
 const prisma = new PrismaClient();
 
 const {
@@ -17,79 +19,59 @@ const ws = websocket.initServer({
 ws.on('message', async function incoming(data: any) {
   const res = JSON.parse(data.toString());
   if (res.block) {
-    console.log('@@@ res.block \n', res.block);
-    console.log(res.block);
+    //console.log('@@@ res.block \n', res.block);
 
     const blockStatus = await blocks.getBlockStatus({ hash: res.block.id });
     console.log('@@@ status', blockStatus);
 
     const blockTxs = await blocks.getBlockTxs({ hash: res.block.id });
-    console.log('@@@ blockTxs', blockTxs);
-    console.log('@@@ blockTxs JSON', JSON.stringify(blockTxs));
+    //console.log('@@@ blockTxs', blockTxs);
+    //console.log('@@@ blockTxs JSON', JSON.stringify(blockTxs));
 
     for (const tx of blockTxs) {
-      if (1 === tx.vin.length) {
-        const vin = tx.vin[0];
-        for (const item of tx.vout) {
-          if (vin.prevout) {
-            const origin = vin.prevout.scriptpubkey;
-            const destination = item.scriptpubkey;
-            const amount = item.value;
+      const vin = tx.vin[0];
+      for (const item of tx.vout) {
+        if (vin.prevout) {
+          const origin = vin.prevout.scriptpubkey_address;
+          const destination = item.scriptpubkey_address;
+          const amount = item.value;
 
-            const count = await getEscrowCount();
-            for (let index = 0; index < count; index++) {
-              const escrow = await getEscrow(BigInt(index));
+          const count = await getEscrowCount();
+          //console.log('@@@ getEscrowCount ', count);
 
-              const request = await getRequest(escrow.originatorBitcoinAddress, escrow.requestNumber);
+          for (let index = 0; index < count; index++) {
+            const escrow = await getEscrow(BigInt(index));
+            //console.log('@@@ escrow ', escrow);
 
-              if (
-                escrow.originatorBitcoinAddress === origin &&
-                escrow.destinationBitcoinAddress === destination &&
-                request.amount === BigInt(amount)
-                //todo blocktimestamp less than request expiry
-              ) {
-                await fulfillRequest(BigInt(0)); //await fulfillRequest(BigInt(index));//todo for index 0
+            const request = await getRequest(escrow.originatorBitcoinAddress, escrow.requestNumber);
+            //console.log('@@@ request ', request);
 
-                await prisma.transactions.create({
-                  data: {
-                    origin: escrow.originatorBitcoinAddress,
-                    request_number: Number(escrow.requestNumber),
-                    quote_number: Number(escrow.quoteNumber),
-                    amount: Number(amount),
-                    destination: escrow.destinationBitcoinAddress,
-                    expiry: Number(request.expiry),
-                    fulfilled: true,
-                  },
-                });
-              }
+            let encodeOriginatorBitcoinAddress = base58.encode(Buffer.from(escrow.originatorBitcoinAddress, 'hex'));
+            let encodeDestinationBitcoinAddress = base58.encode(Buffer.from(escrow.destinationBitcoinAddress, 'hex'));
+
+            if (
+              encodeOriginatorBitcoinAddress === origin &&
+              encodeDestinationBitcoinAddress === destination &&
+              request.amount === BigInt(amount) &&
+              res.block.timestamp < request.expiry
+            ) {
+              await fulfillRequest(BigInt(index));
+
+              await prisma.transactions.create({
+                data: {
+                  origin: escrow.originatorBitcoinAddress,
+                  request_number: Number(escrow.requestNumber),
+                  quote_number: Number(escrow.quoteNumber),
+                  amount: Number(amount),
+                  destination: escrow.destinationBitcoinAddress,
+                  expiry: Number(request.expiry),
+                  fulfilled: true,
+                },
+              });
             }
           }
         }
       }
     }
   }
-  // if (res.mempoolInfo) {
-  //   console.log('@@@ res.mempoolInfo \n');
-  //   console.log(res.mempoolInfo);
-  // }
-  // if (res.transactions) {
-  //   console.log('@@@ res.transactions \n');
-  //   console.log(res.transactions);
-  // }
-  // if (res['mempool-blocks']) {
-  //   console.log('@@@ res.mempool-blocks \n');
-  //   console.log(res['mempool-blocks']);
-  // }
 });
-
-// async function main() {}
-
-// main()
-//   .then(async () => {
-//     await prisma.$disconnect();
-//   })
-//   .catch(async (e) => {
-//     console.error(e);
-//     await prisma.$disconnect();
-//     process.exit(1);
-//   });
